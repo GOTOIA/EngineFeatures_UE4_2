57cab6ca0171e9a0e3f59d5c5753923615d1200c
nglconsulting <nico.nglconsulting@gmail.com>
2019-09-18 15:13:56 +0200

    Converting a character BP to C++ Refactoring de BP_TPCharacter Suppression des component Mesh, Camera, ChildActor. Création de la classe CAMannequin, set component Camera, FPMesh, et GUN Load BP_GUN Référence(Gun = GetWorld()->SpawnActor<ACGUN>(GunBlueprint);) //TODO Reparent GUN

---
+++ Source/EnginFeaturesUE4/Public/Character/CMannequin.h
@@ -0,0 +1,51 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+
+
+#include "GameFramework/Character.h"
+#include "CoreMinimal.h"
+#include "CMannequin.generated.h"
+
+
+
+UCLASS()
+class ENGINFEATURESUE4_API ACMannequin : public ACharacter
+{
+	GENERATED_BODY()
+
+public:
+	// Sets default values for this character's properties
+	ACMannequin();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:	
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	// Called to bind functionality to input
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UPROPERTY(EditDefaultsOnly, Category = "Setup")
+		TSubclassOf<class ACGUN> GunBlueprint;
+
+	UFUNCTION(BlueprintCallable, Category = "Weapon")
+		void Fire();
+
+private:
+	/** Pawn mesh: 1st person view (arms; seen only by self) */
+	UPROPERTY(VisibleDefaultsOnly, Category = Mesh)
+		class USkeletalMeshComponent* Mesh1P;
+
+	/** First person camera */
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		class UCameraComponent* FirstPersonCameraComponent;
+
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Camera, meta = (AllowPrivateAccess = "true"))
+		class ACGUN * Gun;
+
+};
